af7ea8ad4643a5650bffde648cc1fc8f41aae15d
stevendreed <steven.daniel.reed@gmail.com>
2024-01-08 19:54:35 -0800

    added Product/id

--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,14 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id:
+    {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    
   },
   {
     sequelize,
