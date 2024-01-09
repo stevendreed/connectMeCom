94e7eedf280af4ce7653599a2c2f91e1bb66e5a5
stevendreed <steven.daniel.reed@gmail.com>
2024-01-09 11:18:32 -0800

    added id, product_id, && tag_id cols to ProductTag

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,21 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id:
+    {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id:
+    {
+      type: DataTypes.INTEGER,
+    },
+    tag_id:
+    {
+      type: DataTypes.INTEGER
+    }
   },
   {
     sequelize,
